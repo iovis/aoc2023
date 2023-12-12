c3ae9b0f543838a35327517ac4ea23167db38e56
David Marchante <davidmarchan@gmail.com>
2023-12-12 09:12:20 -0500

    day12 init

---
+++ day12/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("p1 = {:?}", p1(input));
+    // println!("p2 = {:?}", p2(input));
+}
+
+/// '.' -> operational
+/// '#' -> damaged
+/// '?' -> unknown
+///
+/// How many differet possible arrangements per row?
+/// Sum them
+fn p1(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1_test() {
+        let input = indoc::indoc! {"
+            ???.### 1,1,3
+            .??..??...?##. 1,1,3
+            ?#?#?#?#?#?#?#? 1,3,1,6
+            ????.#...#... 4,1,1
+            ????.######..#####. 1,6,5
+            ?###???????? 3,2,1
+        "};
+
+        assert_eq!(p1(input), 21);
+    }
+}
